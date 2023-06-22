a2b9c620363900392aa91fc6686e00320ea79fd2
Cdunington <cfDunington@gmail.com>
2023-06-22 10:27:30 -0700

    Cleaned up Movement
    just reduced the blank spacing to make it a little more compact

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,33 +6,50 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     GameObject player;
-
-   
+    bool isRolling;
+    [SerializeField]
+    Rigidbody2D _rigidbody;
     void Update()
     {
-        if (Input.GetButton("s")) 
+        PlayerMovement();
+        DodgeRoll();
+    }
+
+    public void PlayerMovement()
+    {
+        //handles the left,right,up and down
+        if (Input.GetButton("s"))
         {
 
             transform.Translate(0, -.01f, 0);
 
 
         }
-
-
         if (Input.GetButton("w"))
         {
-            transform.Translate(0,.01f,0);
+            transform.Translate(0, .01f, 0);
         }
-
         if (Input.GetButton("a"))
         {
             transform.Translate(-.01f, 0, 0);
         }
-
-
         if (Input.GetButton("d"))
         {
             transform.Translate(.01f, 0, 0);
         }
+
+        
+    }
+
+    private void DodgeRoll()
+    {
+        if (isRolling == false)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                
+            }
+        }
+
     }
 }
