898c8279207c2679bb036e0ce76a44a2e3a27169
Cdunington <cfDunington@gmail.com>
2023-06-22 13:22:00 -0700

    Updated the player controller
    the commented out part is the old one, we can decide which is better later

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,38 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Diagnostics;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     GameObject player;
-
-   
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float dodgeSpeed; 
+    private State state;
+    Vector3 dodgeDir;
+    [SerializeField]
+    Rigidbody2D rigidbody;
+   private enum State
+    {
+        Normal, 
+        DodgeRoll,
+    }
+    private void Awake()
+    {
+        state = State.Normal;
+    }
     void Update()
     {
-        if (Input.GetButton("s")) 
+        switch (state)
         {
-
-            transform.Translate(0, -.01f, 0);
-
-
+            case State.Normal:
+                HandleMovement();
+                HandleDodgeState();
+                break;
+            case State.DodgeRoll:
+                HandleDodgeRoll();
+                break;
         }
+        
+    }
+
+    private void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+        rigidbody.velocity = movement * speed;
+        //if (Input.GetButton("down"))
+        //{
+        //  transform.Translate(0, -speed, 0);
+
+        //}
+        //if (Input.GetButton("up"))
+        //{
+        //    transform.Translate(0, speed, 0);
+
+        //}
+        //if (Input.GetButton("left"))
+        //{
+        //    transform.Translate(-speed, 0, 0);
+
+        //}
+        //if (Input.GetButton("right"))
+        //{
+        //    transform.Translate(speed, 0, 0);
+
+        //} 
 
 
-        if (Input.GetButton("w"))
+    } 
+
+    private void HandleDodgeState()
+    {
+        if (Input.GetButton("dodge"))
         {
-            transform.Translate(0,.01f,0);
-        }
-
-        if (Input.GetButton("a"))
-        {
-            transform.Translate(-.01f, 0, 0);
-        }
-
-
-        if (Input.GetButton("d"))
-        {
-            transform.Translate(.01f, 0, 0);
+            state = State.DodgeRoll;
+           
+            state = State.Normal;
         }
     }
+    private void HandleDodgeRoll()
+    {
+        
+    }
+    
 }
